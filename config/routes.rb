af6f7460bb9774e9b43d4d152b0c3603f7373d62
Nikhil.Singh <nikhil22897@gmail.com>
2021-10-27 19:17:02 +0530

    feat: Analytics Dash: Added analytics dashboard module

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'analytics_dash/index'
   get 'exercise_computation/index'
   match 'weight_logs/index', to: 'weight_logs#index', via: [:get, :post]
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'dashboard#index'
   Rails.application.routes.draw do
+  get 'analytics_dash/index'
     devise_for :users, controllers: {
       sessions: 'users/sessions'
     }
